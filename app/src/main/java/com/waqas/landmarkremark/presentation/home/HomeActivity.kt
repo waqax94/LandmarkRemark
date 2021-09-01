e99aff93802dc56a540133e12300564c4a053420
waqax94 <waqax94@gmail.com>
2021-09-01 20:58:11 +1000

    - Location retrieval

--- app/src/main/java/com/waqas/landmarkremark/presentation/home/HomeActivity.kt
+++ app/src/main/java/com/waqas/landmarkremark/presentation/home/HomeActivity.kt
@@ -1,18 +1,163 @@
 package com.waqas.landmarkremark.presentation.home
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.waqas.landmarkremark.BuildConfig
 import com.waqas.landmarkremark.R
 import com.waqas.landmarkremark.databinding.ActivityHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var binding: ActivityHomeBinding
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private var currentLocation: Location? = null
+    private lateinit var mMap : GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initMap(savedInstanceState)
+        showMyLocation()
+        saveNotes()
+    }
+    private fun initMap(savedInstanceState: Bundle?) {
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        var mapViewBundle: Bundle? = null
+
+        if (savedInstanceState != null) {
+            mapViewBundle = savedInstanceState.getBundle(BuildConfig.MAPS_KEY)
+        }
+
+        binding.mapView.onCreate(mapViewBundle)
+
+        binding.mapView.getMapAsync(this)
+    }
+
+    private fun showMyLocation(){
+        binding.homeMyLocBtn.setOnClickListener {
+            getLocation()
+        }
+    }
+
+    private fun saveNotes(){
+        binding.saveNotesHereButton.setOnClickListener {
+            onMapReady(mMap)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.mapView.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        binding.mapView.onStop()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        getLocation()
+        binding.mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding.mapView.onPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding.mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        binding.mapView.onLowMemory()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        var mapViewBundle = outState.getBundle(BuildConfig.MAPS_KEY)
+        if (mapViewBundle == null) {
+            mapViewBundle = Bundle()
+            outState.putBundle(BuildConfig.MAPS_KEY, mapViewBundle)
+        }
+        binding.mapView.onSaveInstanceState(mapViewBundle)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        if(currentLocation != null){
+            val latLng = LatLng(currentLocation?.latitude!!, currentLocation?.longitude!!)
+
+            val options = MarkerOptions().position(latLng).title("Current Location")
+
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 18.0F))
+
+            mMap.addMarker(options)
+        }
+    }
+
+    private fun getLocation(){
+        if (ActivityCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(
+                            Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.ACCESS_COARSE_LOCATION
+                    ),
+                    LOCATION_REQUEST_CODE
+            )
+            return
+        }
+
+        fusedLocationClient.lastLocation.addOnSuccessListener {
+            if(it != null){
+                currentLocation = it
+                onMapReady(mMap)
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == LOCATION_REQUEST_CODE && grantResults.isNotEmpty()){
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                getLocation()
+            }
+            else{
+                Toast.makeText(this@HomeActivity, getString(R.string.permission_denied), Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    companion object {
+        private const val LOCATION_REQUEST_CODE = 111
     }
 }
